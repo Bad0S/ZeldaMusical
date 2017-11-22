9f77c009bd503c6bb663b93ab4f8b7b0984014e3
ETU\v.bourgogne <v.bourgogne@rubika-edu.com>
2017-11-22 15:38:18 +0100

    Démo pour tester les contrôles et GitHub/SourceTree

---
+++ Tests Rythm/Assets/Scripts/DistanceCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCheck : MonoBehaviour {
+    public Collider2D Player;
+    public AudioSource BGMusic;
+    public AudioClip MusicToPlay;
+    public AudioClip MusicToPlayExit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D Player)
+    { 
+        if (BGMusic.clip !=  MusicToPlay)
+        {
+            BGMusic.clip = MusicToPlay;
+            BGMusic.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D Player)
+    {
+        if (BGMusic.clip != MusicToPlayExit)
+        {
+            BGMusic.clip = MusicToPlayExit;
+            BGMusic.Play();
+        }
+    }
+}
---
+++ Tests Rythm/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private Rigidbody2D body;
+	public float MovSpeed;
+	public PlayerAttack Swing;
+	public PlayerAttack Thrust;
+    public AudioClip SwingSound;
+    public AudioClip ThrustSound;
+    private AudioSource Source;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		body = GetComponent<Rigidbody2D> ();
+        Source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+		body.velocity = new Vector2 (horizontal * MovSpeed, vertical * MovSpeed);
+		Attack ();
+	}
+	void Attack()
+	{
+		if (Input.GetMouseButton (0) == true) 
+		{
+            Source.clip = SwingSound;
+            Source.Play();
+			Swing.gameObject.SetActive (true);
+			StartCoroutine (DisableObject (Swing.gameObject,.1f));
+			return;
+		}
+		if (Input.GetMouseButton (1) == true)
+		{
+            Source.clip = ThrustSound;
+            Source.Play();
+			Thrust.gameObject.SetActive (true);
+			StartCoroutine (DisableObject (Thrust.gameObject,.1f));
+			return;
+		} 
+	}
+
+	IEnumerator DisableObject(GameObject obj, float time)
+	{
+		yield return new WaitForSeconds(time);
+		obj.SetActive (false);
+	}
+}
